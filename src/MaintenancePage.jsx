db61d08438b3e2f2565548368189913b4cdce699
parameshwar-arroju <parameshwarshiva119@gmail.com>
2024-04-18 16:31:41 +0530

    added loader

--- src/MaintenancePage.jsx
+++ src/MaintenancePage.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const MaintenancePage = () => {
     return (
-        <div className="container-fluid d-flex align-items-center vh-100 maintenance-page">
-            <div className="container text-center">
-                <h1 className='display-2'>Website Under Maintenance</h1>
-                <p className='fs-5 w-sm-50 mx-auto'>Sorry for the inconvenience. We are currently performing maintenance on our website. Please check back later.</p>
-            </div>
+        <div className="container-fluid d-flex align-items-center justify-content-center vh-100 maintenance-page">
+            {/* <div className="container text-center"> */}
+            {/*     <h1 className="display-2">Website Under Maintenance</h1> */}
+            {/*     <p className="fs-5 w-sm-50 mx-auto"> */}
+            {/*         Sorry for the inconvenience. We are currently performing */}
+            {/*         maintenance on our website. Please check back later. */}
+            {/*     </p> */}
+            {/* </div> */}
+            <div className="loader"></div>
         </div>
     );
 };
